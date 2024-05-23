036b700625151caf2e306e923e28bcd5503d3c65
lddtwo456 <72422028+lddtwo456@users.noreply.github.com>
2024-05-23 11:40:29 -0400

    building quaternion from array refactor for consistency

--- Quaternion.py
+++ Quaternion.py
@@ -2,11 +2,11 @@ import numpy as np
 
 # custom quaternion class that's probably very slow but good for learning
 class quaternion:
-  def __init__(self, w, x=0, y=0, z=0):
-    if (isinstance(w, np.ndarray)):
-      self.q = w
-    else:
-      self.q = np.array([w,x,y,z])
+  def __init__(self, w, x, y, z):
+    self.q = np.array(w, x, y, z)
+
+  def fromArray(array):
+    return quaternion(array[0], array[1], array[2], array[3])
 
   def fromAxisAngle(theta, x, y, z):
     # constructor for making quaternions from axis angle representation
@@ -55,12 +55,12 @@ class quaternion:
 
   def inverse(self):
     divisor = self.q[0]**2 + self.q[1]**2 + self.q[2]**2 + self.q[3]**2
-    return quaternion(np.array([self.q[0], -self.q[1], -self.q[2], -self.q[3]])/divisor)
+    return quaternion.fromArray(np.array([self.q[0], -self.q[1], -self.q[2], -self.q[3]])/divisor)
   
   def normalized(self):
     q = self.q
     m = np.sqrt(q[0]**2 + q[1]**2 + q[2]**2 + q[3]**2)
-    return quaternion(self.q / m)
+    return quaternion.fromArray(self.q / m)
 
   def __mul__(self, other):
     q = self.q
