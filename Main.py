e4082a0238da6794cadfe30f1e795f0c0566d38a
lddtwo456 <72422028+lddtwo456@users.noreply.github.com>
2024-05-24 09:48:15 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,12 +2,11 @@ import numpy as np
 from Quaternion import quaternion
 from Vector3D import v3d
 
-p = v3d(0, 2, 0)
+p = v3d(2, 2, 2)
 q = quaternion.fromEulerDeg(v3d(90, 0, 0))
 
 print(q)
 
 v = v3d(2, 3, 5)
-print(f"{v.x} {v.y} {v.z}")
 
 print(q.applyToPoint(p))
