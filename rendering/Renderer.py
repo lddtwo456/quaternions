864766ed753fa7079832a38eb6841a7e285dd9b6
lddtwo456 <72422028+lddtwo456@users.noreply.github.com>
2024-06-05 09:13:05 -0400

    FIXED

--- rendering/Renderer.py
+++ rendering/Renderer.py
@@ -26,4 +26,4 @@ class Renderer:
     ObjectsHandler.getTransforms(Renderer.cam)
 
     for obj in ObjectsHandler.objects:
-      Renderer.vertShader.transformVBO(Renderer.queue, (obj.VBO.size/32,), (None,), obj.VBO, obj.matrix, obj.outVBO, np.uint32(obj.vert_count)).wait()
+      Renderer.vertShader.transformVBO(Renderer.queue, (np.uint32(obj.VBO.size/32),), None, obj.VBO, obj.matrix, obj.outVBO, np.uint32(obj.vert_count)).wait()
