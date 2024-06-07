0250ccb56c597a1f7e2db3ec32216fcc59e271e2
lddtwo456 <72422028+lddtwo456@users.noreply.github.com>
2024-06-07 11:26:12 -0400

    Update Renderer.py

--- rendering/Renderer.py
+++ rendering/Renderer.py
@@ -25,5 +25,23 @@ class Renderer:
     # get transform matrices from local coords to projected points
     ObjectsHandler.getTransforms(Renderer.cam)
 
+    VBOs_list = []
+    Mats_list = []
+    Offs_list = []
+    offset = 0
     for obj in ObjectsHandler.objects:
-      Renderer.vertShader.transformVBO(Renderer.queue, (np.uint32(obj.VBO.size/32),), None, obj.VBO, obj.matrix, obj.outVBO, np.uint32(obj.vert_count)).wait()
+      Offs_list.append(offset)
+      VBOs_list.append(obj.VBO)
+      Mats_list.append(obj.matrix)
+      offset += obj.vert_count
+
+    VBOs_mat = np.array(VBOs_list, dtype=np.float32).flatten('C')
+    Mats_mat = np.array(Mats_list, dtype=np.float32).flatten('C')
+    Offs_mat = np.array(Offs_list, dtype=np.uint32)
+
+    VBOs = cl.Buffer(Renderer.ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=VBOs_mat)
+    cam_transformed_VBOs = cl.Buffer(Renderer.ctx, cl.mem_flags.READ_WRITE, len(VBOs_mat))
+    Mats = cl.Buffer(Renderer.ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=Mats_mat)
+    Offs = cl.Buffer(Renderer.ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=Offs_mat)
+
+    Renderer.vertShader.applyTransformMatrixToVBO(Renderer.queue, (np.uint32(VBOs_mat.size/32),), None, VBOs, Mats, cam_transformed_VBOs, Offs)
